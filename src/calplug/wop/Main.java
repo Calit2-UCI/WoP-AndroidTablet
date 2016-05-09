62eb2a280f459bcf4c5bbb206e932ef75d3b6368
Zeph <zeph1912@users.noreply.github.com>
2016-05-09 12:02:15 -0700

    change server address

---
+++ src/calplug/wop/Main.java
@@ -0,0 +1,135 @@
+package calplug.wop;
+
+import java.util.ArrayList;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import calplug.wop.R;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.LightingColorFilter;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.Window;
+import android.view.WindowManager;
+ 
+public class Main extends Activity {
+  
+    private EditText username;  
+    private EditText password;  
+
+	private Button login;
+	private Button register;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//Remove title bar
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+		//Remove notification bar
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		
+		//set the activity's view
+		setContentView(R.layout.login);
+		
+		username = (EditText)findViewById(R.id.editText1);  
+        password = (EditText)findViewById(R.id.editText2);
+		username.getBackground().setAlpha(142);
+		password.getBackground().setAlpha(142);
+		username.setTextColor(Color.DKGRAY);
+		password.setTextColor(Color.DKGRAY);
+		
+		addListenerOnButton();
+		
+		//solve the NetworkOnMainThreadException for now, need a better solution later
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy); 
+	}
+ 
+	private void addListenerOnButton() {
+ 
+		final Context context = this;
+
+		
+		login = (Button) findViewById(R.id.button2);
+		
+		register = (Button) findViewById(R.id.button1);
+		 
+		login.setTextColor(Color.WHITE);
+		login.getBackground().setColorFilter(new LightingColorFilter(0x006600, 0x006600));
+		register.setTextColor(Color.WHITE);
+		register.getBackground().setColorFilter(new LightingColorFilter(0x006600, 0x006600));
+		
+		
+		register.setOnClickListener(new OnClickListener() {
+ 
+			public void onClick(View arg0) {
+			    Intent intent = new Intent(context, Register.class);
+                startActivity(intent);
+			}
+ 
+		});
+ 
+		login.setOnClickListener(new OnClickListener() {
+ 
+			public void onClick(View arg0) {
+				
+				
+				ArrayList<NameValuePair> postParameters = new ArrayList<NameValuePair>();  
+				postParameters.add(new BasicNameValuePair("username", username.getText().toString()));  
+				postParameters.add(new BasicNameValuePair("password", password.getText().toString())); 
+				String response = null; 
+				
+				try {  
+					response = CustomHttpClient.executeHttpPost("http://128.195.185.104:8080/WallOfPower4AndroidAPP/check1.php", postParameters);  
+					String res=response.toString();
+					
+					//Toast.makeText(Main.this, "                      md5=" + res , Toast.LENGTH_SHORT).show();  
+			              
+					
+					res= res.replaceAll("\\s+","");
+					
+					//log into control mode
+					if(res.equals("1"))
+					{  
+		                System.out.println("You are in control mode.");  
+		                Intent intent = new Intent(context, ControlMode.class);
+		                //Intent intent = new Intent(context, MjpegSample.class);
+		                startActivity(intent); 
+					}
+					//invalid user
+					if(res.equals("0"))  
+					{  
+		                 Toast.makeText(Main.this,"Invalid user. Please login again." , Toast.LENGTH_SHORT).show();  
+					}
+									
+				
+			}catch (Exception e) {
+					username.setText(e.toString());  
+					}
+				/*
+                System.out.println("You are in control mode.");  
+                Intent intent = new Intent(context, ControlMode.class);
+                startActivity(intent);
+                */
+			}
+ 		});
+ 
+	}
+	public void onPause() {
+		super.onPause();
+	}
+
+ 
+}
+
+
---
+++ src/calplug/wop/ObserveMode.java
@@ -0,0 +1,161 @@
+package calplug.wop;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import calplug.wop.R;
+import de.mjpegsample.MjpegView.MjpegInputStream;
+import de.mjpegsample.MjpegView.MjpegView;
+
+public class ObserveMode extends Activity {
+	private MjpegView mv;
+	private RelativeLayout rl;
+	private WebView stats;
+	private Button Logout;
+	private Handler mHandler = new Handler();
+	private int PD1=0;
+	private int PD2=0;
+	private int PD3=0;
+	private int All=0;
+	private ImageView  icon;
+	
+	public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        rl = new RelativeLayout(this);
+        String URL = "http://root:admin@128.200.55.90:82/axis-cgi/mjpg/video.cgi?resolution=640x480"; 
+        
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+      	//Remove notification bar
+      	this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+      	this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        Resources res = getResources(); //resource handle
+    	Drawable drawable = res.getDrawable(R.drawable.background); 
+    	
+        rl.setBackgroundDrawable(drawable);
+        
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+       	     RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        	layoutParams.setMargins(0, 50, 0, 0);
+        	layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        	icon = new ImageView(this);
+            icon.setLayoutParams(layoutParams);
+            icon.setImageResource(R.drawable.largelogo);
+            
+            layoutParams = new RelativeLayout.LayoutParams(
+              	     RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+               	layoutParams.setMargins(770, 200, 0, 0);
+               	layoutParams.height=400;
+               	layoutParams.width=380;
+               	//layoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+               	stats = new WebView(this);
+                   stats.setLayoutParams(layoutParams);
+           		stats.loadUrl("http://128.195.185.104:8080/WOP_LastVersion/androidob.html");
+        		stats.setWebChromeClient(new WebChromeClient());
+                stats.addJavascriptInterface(new DemoJavaScriptInterface(), "stats");
+                   WebSettings websetting = stats.getSettings();
+                   websetting.setBuiltInZoomControls(false);
+                   websetting.setJavaScriptEnabled(true);//OPEN JavaScript for WEB
+                   stats.setVerticalScrollBarEnabled(false);
+                   stats.setOnTouchListener(new View.OnTouchListener() {
+                   	
+       				public boolean onTouch(View v, MotionEvent event) {
+       					return true;
+       				}
+       			});
+
+       layoutParams = new RelativeLayout.LayoutParams(
+       	     RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        	layoutParams.setMargins(50, 180, 0, 0);
+        	layoutParams.height=480;
+        	layoutParams.width=640;
+        	mv = new MjpegView(this);
+            mv.setLayoutParams(layoutParams);
+            
+            
+            Logout = new Button(this);
+            Logout.setText("Logout");
+            Logout.setOnClickListener(new OnClickListener() {
+            	 
+    			public void onClick(View arg0) {
+    				String response=null;
+    				try {  
+    					response = CustomHttpClient.executeHttpGet("http://128.195.185.104:8080/WOP_LastVersion/androidlogout.php");  
+    				
+    			}catch (Exception e) {  
+    					  
+    					System.out.println(e.toString()+response);
+    					}
+    				Intent intent = new Intent(Intent.ACTION_MAIN);
+    				intent.addCategory(Intent.CATEGORY_HOME);
+    				intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+    				startActivity(intent);
+    			}
+     
+    		});
+            layoutParams = new RelativeLayout.LayoutParams(
+             	     RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+            layoutParams.setMargins(1200, 20, 0, 0);
+            Logout.setLayoutParams(layoutParams);
+            
+            rl.addView(icon);
+            rl.addView(stats);
+            rl.addView(mv);
+            rl.addView(Logout);
+            setContentView(rl);
+            //addListenerOnButton();
+            mv.setSource(MjpegInputStream.read(URL));
+            //mv.setDisplayMode(MjpegView.SIZE_BEST_FIT);
+            mv.showFps(true); 
+	}
+	//Javascript interface here
+	final class DemoJavaScriptInterface {
+
+        DemoJavaScriptInterface() {
+        }
+        /**
+         * This is not called on the UI thread. Post a runnable to invoke
+         * loadUrl on the UI thread.
+         */
+        public void clickOnAndroid() {
+            mHandler.post(new Runnable() {
+                public void run() {
+                	stats.loadUrl("javascript:updateValue("+PD1+","+PD2+","+PD3+","+All+")");
+                }
+            });
+            //return PD1;
+        }  
+    }
+    
+    final class MyWebChromeClient extends WebChromeClient {
+        @Override
+        public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+            //Log.d(LOG_TAG, message);
+            result.confirm();
+            return true;
+        }
+    }
+	public void onPause() {
+		super.onPause();
+		mv.stopPlayback();
+	}
+}
